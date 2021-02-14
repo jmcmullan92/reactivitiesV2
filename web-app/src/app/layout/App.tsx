58912b0880ce88232c2face7807143e5794bbb82
Jonathan McMullan <jmcmullan92@outlook.com>
2021-02-14 01:41:35 +0000

    End of section 5 - creating CRUD operations in web-app for activities

---
+++ web-app/src/app/layout/App.tsx
@@ -0,0 +1,81 @@
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import { Container } from "semantic-ui-react";
+import { Activity } from "./models/activity";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import {v4 as uuid} from 'uuid';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
+
+  const [editMode, setEditMode] = useState(false); //inferring type boolean from false
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find((x) => x.id === id));
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity){
+    activity.id
+      ? setActivities([
+          ...activities.filter((x) => x.id !== activity.id),
+          activity,
+        ])
+      : setActivities([...activities, {...activity, id: uuid()}]);
+      setEditMode(false);
+      setSelectedActivity(activity);
+
+  }
+
+  function handleDeleteActivity(id: string){
+    setActivities([...activities.filter(x => x.id !== id)])
+  }
+
+  return (
+    // <> = shortcut version of using a Fragment which is like using a div but hides it from html
+    <>
+      <NavBar openForm={handleFormOpen}/>
+      <Container style={{ margin: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+
+      <ul></ul>
+    </>
+  );
+}
+
+export default App;
