753541078cd8ab8d60625b51ba8074f7ccf7d55a
Jonathan McMullan <jmcmullan92@outlook.com>
2021-02-16 01:57:14 +0000

    End of section 7 - adding mobx

--- web-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ web-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,61 +1,35 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
 import { Grid } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
-interface Props {
-  activities: Activity[];
-  selectedActivity: Activity | undefined;
-  selectActivity: (id: string) => void;
-  cancelSelectActivity: () => void;
-  editMode: boolean;
-  openForm: (id: string) => void;
-  closeForm: () => void;
-  createOrEdit: (activity: Activity) => void;
-  deleteActivity: (id: string) => void;
-  submitting: boolean;
-}
 
-export default function ActivityDashboard({
-  activities,
-  selectedActivity,
-  selectActivity,
-  cancelSelectActivity,
-  editMode,
-  openForm,
-  closeForm,
-  createOrEdit,
-  deleteActivity,
-  submitting
-}: Props) {
+function ActivityDashboard() {
+
+  const {activityStore} = useStore();
+  const {selectedActivity, editMode} = activityStore;
+
   return (
     /* semantic ui grid systeam has 16 unlike bootstraps 12 */
     <Grid>
       <Grid.Column width="10">
-        <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} submitting={submitting} />
+        <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
         {selectedActivity && !editMode && (
-          <ActivityDetails
-            activity={selectedActivity}
-            cancelSelectActivity={cancelSelectActivity}
-            openForm={openForm}
-          />
-        )}
+          <ActivityDetails/>)}
         {editMode && (
-          <ActivityForm
-            closeForm={closeForm}
-            activity={selectedActivity}
-            createOrEdit={createOrEdit}
-            submitting={submitting}
-          />
+          <ActivityForm />
         )}
       </Grid.Column>
     </Grid>
   );
 }
+
+export default observer(ActivityDashboard);
 
 // && in here means everything to the right of this && will execute only if what's on the left is true
 // so activity details will only load if activities[0] exists/ is true
--- web-app/src/features/activities/form/ActivityForm.tsx
+++ web-app/src/features/activities/form/ActivityForm.tsx
@@ -1,20 +1,16 @@
+import { observer } from "mobx-react-lite";
 import React, { ChangeEvent, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
 
-interface Props {
-  activity: Activity | undefined;
-  closeForm: () => void;
-  createOrEdit: (activity: Activity) => void;
-  submitting: boolean
-}
 
-export default function ActivityForm({
-  activity: selectedActivity,
-  closeForm,
-  createOrEdit,
-  submitting
-}: Props) {
+
+export default observer(function ActivityForm() {
+
+  const {activityStore} = useStore();
+  const {selectedActivity, closeForm, createActivity, updateActivity, loading} = activityStore;
+
   const initialState = selectedActivity ?? {
     id: "",
     title: "",
@@ -28,7 +24,7 @@ export default function ActivityForm({
   const [activity, setActivity] = useState(initialState);
 
   function handleSubmit() {
-    createOrEdit(activity);
+    activity.id ? updateActivity(activity) : createActivity(activity)
   }
 
   function handleInputChange(
@@ -78,7 +74,7 @@ export default function ActivityForm({
           name="venue"
           onChange={handleInputChange}
         />
-        <Button floated="right" positive type="submit" content="Submit" loading={submitting} />
+        <Button floated="right" positive type="submit" content="Submit" loading={loading} />
         <Button
           onClick={closeForm}
           floated="right"
@@ -88,4 +84,4 @@ export default function ActivityForm({
       </Form>
     </Segment>
   );
-}
+})
