af037a99840f7828bd7e61d8d180da31c7be76a1
Jonathan McMullan <jmcmullan92@outlook.com>
2021-03-18 22:54:09 +0000

    End of section 14 - mapping and attendances for activities

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace Domain
@@ -8,8 +9,9 @@ namespace Domain
         public string DisplayName {get; set;}
 
         public string Bio { get; set; }
+        
+        public ICollection<ActivityAttendee> Activities { get; set; }
 
-        
-        
+
     }
 }
